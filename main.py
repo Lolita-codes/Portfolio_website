a6d2e88a698c61bc311bbb5b481d6e3e90988d88
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2022-08-09 15:31:26 +0100

    Changes made

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, url_for
 import smtplib, ssl
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, validators
@@ -46,10 +46,8 @@ def check_resume():
     return render_template('resume.html')
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='favicon.ico'))
 
 
 if __name__ == '__main__':
